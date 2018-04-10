e562d29f0f704a32fc47cbd4a8bd29a872396f88
MichiFrech <michifrech2000@gmail.com>
2018-04-10 08:07:43 +0200

    chart

--- TMS/TMS/views/HomeView.xaml.cs
+++ TMS/TMS/views/HomeView.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace TMS.views
 {
@@ -29,7 +31,30 @@ namespace TMS.views
             dgrid_today.ItemsSource = InitializeAllTasks();
             dgrid_week.ItemsSource = InitializeAllTasks();
             dgrid_month.ItemsSource = InitializeAllTasks();
+
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Tasks",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 , 7, 4, 6, 5, 2 , 7, 4, 6, 5, 2 , 7, 4, 6, 5, 2 , 7, 4, 6, 5, 2 , 7, 4, 6, 5, 2 , 7, 3, 5 },
+                    LineSmoothness = 0
+                }
+            };
+
+            Labels = new[] { "DAY 1", "DAY 2", "DAY 3", "DAY 4", "DAY 5", "DAY 6", "DAY 7", "DAY 8", "DAY 9", "DAY 10", "DAY 11", "DAY 12", "DAY 13", "DAY 14", "DAY 15", "DAY 16", "DAY 17", "DAY 18", "DAY 19", "DAY 20", "DAY 21", "DAY 22", "DAY 23", "DAY 24", "DAY 25", "DAY 26", "DAY 27", "DAY 28", "DAY 29", "DAY 30", "DAY 31", "DAY 32" };
+            YFormatter = value => value.ToString("");
+
+            //modifying the series collection will animate and update the chart
+
+            //modifying any series values will also animate and update the chart
+
+            DataContext = this;
         }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
 
         public ObservableCollection<Task> InitializeAllTasks()
         {
